3d755ecd1807646484dcdb5bc63e1bb927351596
deividfuzari <152648702+deividfuzari@users.noreply.github.com>
2026-05-29 18:35:06 -0300

    colocando validações e trabalhando com seeds

--- 2-model/model/app/models/product.rb
+++ 2-model/model/app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  # validando que o produto esteja presente ou seja not null
+  validates :name, presence: true
+  # validando que o produto não tenha o mesmo nome duas vezes, seja unico
+  validates_uniqueness_of :name
+  # validando que o tamanho maximo de caracteres seja 100
+  validates_length_of :name, maximum: 100
 end
